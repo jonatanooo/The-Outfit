d95dfc25c759057051b4cff58c5b5c2d975660b3
jonatanooo <72115156+jonatanooo@users.noreply.github.com>
2026-08-16 18:40:44 -0600

    Add files via upload

--- THE OUTFIT/MainPage/headerscript.js
+++ THE OUTFIT/MainPage/headerscript.js
@@ -24,7 +24,33 @@ window.addEventListener('scroll', () => {
 
 const menuBtn = document.getElementById('menubtn')
 const menuDesplegable = document.getElementById('menuDesplegable')
+const overlay = document.getElementById('overlay')
 
 menuBtn.addEventListener('click', function() {
+    // el toggle('clase') revisa si la clase ya esta en el elemento, si no la esta la agrega
     menuDesplegable.classList.toggle('abierto');
+    overlay.classList.toggle('activo');
+})
+
+const exitBtn = document.getElementById('cerrarMenu')
+
+exitBtn.addEventListener('click', function() {
+    // el remove('clase') se encarga de quitar la clase sin importar si estaba o no
+    menuDesplegable.classList.remove('abierto');
+    overlay.classList.remove('activo');
+})
+
+document.addEventListener('click', function(event){
+
+    // event.target es el elemento del HTML donde ocurrio el click, ejemplo si hago click en THE OUTFIT seria h1
+    // contains responde la pregunta "¿el elemento donde di click  (event.target) esta dentro de otro elemento (menuDesplegable), ya sea directamente o dentro de alguno de sus hijos?"
+    // devuelve true o false, si hago click en el menu devuelve true, si es fuera del menu es false
+    const  clickDentroDelMenu = menuDesplegable.contains(event.target);
+    const clickEnBotonMenu = menuBtn.contains(event.target);
+
+    if (!clickDentroDelMenu && !clickEnBotonMenu) {
+        // aqui especificamos que si el click fue fuera del menu remueve la clase que abria el menu
+        menuDesplegable.classList.remove('abierto');
+        overlay.classList.remove('activo');
+    }
 })
