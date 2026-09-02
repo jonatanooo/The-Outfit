d2015962a671ee2f9125c2ad83e135501a575f70
jonatanooo <jonatanorellana194@gmail.com>
2026-09-02 00:09:38 -0600

    Agregue el filtro de ropa

--- src/components/FiltroMenu.jsx
+++ src/components/FiltroMenu.jsx
@@ -1,4 +1,5 @@
 "use client"
+import { label } from 'motion/react-client';
 import './FiltroMenu.css'
 import { useState, useEffect, useRef } from 'react';
 
@@ -21,6 +22,43 @@ function FiltroMenu() {
         document.addEventListener('click', ClickFuera)
         return () => document.removeEventListener('click',ClickFuera)
     }, [])
+
+
+    const [abiertoId, setAbiertoId] = useState(null)
+
+    const secciones = [
+        {id: 1, titulo: "Categorias", opciones: ["Zapatos","Camisas","Pantalones","Chaquetas","Faldas"]},
+        {id: 2, titulo: "Colores", opciones: ["Negro", "Blanco", "Verde", "Azul"] },
+        {id: 3, titulo: "Materiales", opciones: ["Algodón", "Poliéster", "Lino"]},
+        {id: 4, titulo: "Tallas", opciones: ["S", "M", "L", "XL", "XXL"]},
+        {id: 5, titulo: "Ordenar Por", opciones: ["Precio: menor a mayor", "Precio: mayor a menor", "Más nuevo"]},
+        
+    ]
+
+const [filtrosSeleccionados, setFiltrosSeleccionados] = useState({})
+
+const toggleFiltro = (seccionTitulo, opcion) => {
+    setFiltrosSeleccionados((prev) => {
+        //sacamos el array actual de esa seccion (o vacio si no existe aun)
+        const actuales = prev[seccionTitulo] || []
+
+        const yaEstaba = actuales.includes(opcion)
+
+        const nuevosValores = yaEstaba
+        ? actuales.filter((item) => item !== opcion) //lo quitamos
+        : [...actuales, opcion] //lo agregamos
+
+        return {
+            ...prev,
+            [seccionTitulo]: nuevosValores
+        }
+    })
+}
+
+const aplicarFiltros =() => {
+    console.log("Filtros a aplicar:", filtrosSeleccionados)
+    setFiltroAbierto(false)
+}
 
     return (
     // fragmento
@@ -52,12 +90,33 @@ function FiltroMenu() {
                 <div className="exitbuttondiv" onClick={() => setFiltroAbierto(false)}>
                     <a><img src="/ICONOS/EXIT.png" alt="salir" className="exitbutton"/></a>
                 </div>
-                <div>
-                    <ul>
-                        <li><a href="" className="part1">MUJERES</a></li>
-                    </ul>
+                <div className="descripcionprenda">
+                <div className="contenedordescripcion">
+                    {secciones.map((seccion) => (
+                        <div className={`acordeon  ${abiertoId === seccion.id? 'activo' : ''}`} key={seccion.id}>
+                            <button className='titulo'
+                            onClick={() => setAbiertoId(abiertoId === seccion.id ? null : seccion.id)}>
+                                {seccion.titulo}
+                                <img src="/ICONOS/Flechaabajo.png" alt="desplegar" id="bajar" />
+                            </button>
+                            <div className='contenido'>
+                                {seccion.opciones.map((opcion) => (
+                                    <label key={opcion} className='checkbox-opcion'>
+                                        <input type="checkbox" 
+                                        checked={filtrosSeleccionados[seccion.titulo]?.includes(opcion) || false}
+                                        onChange={() => toggleFiltro(seccion.titulo, opcion)}/>
+                                        {opcion}
+                                    </label>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                    
+                    <button className='filtrar-categorias' onClick={aplicarFiltros}>FILTRAR</button>
                 </div>
     </div>
+    </div>
+
 
         {filtroAbierto && (
                 // quiere decir que si le hacemos click al overlay se desactiva el menu desplegable
