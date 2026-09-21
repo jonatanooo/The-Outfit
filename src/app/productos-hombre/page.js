7f0d157656a96df5fe76af1c8e7880eb463c1380
Anderson Ezequiel Amaya Canales <ezequielamaya129@gmail.com>
2026-09-20 23:36:55 -0600

    feat: agrega pagina de catalogo de hombres

---
+++ src/app/productos-hombre/page.js
@@ -0,0 +1,25 @@
+import '../productos/WomanPage.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import PaginaCategoria from '@/components/PaginaCategoria'
+
+export default function Home() {
+    return(
+        <>
+        <Header />
+            <main>
+        <section className="imgbanner">
+            <img src="/Fotos/banner-men.jpg" alt="" className="hero-img"/>
+            <h3 className='nombre-seccion'>HOMBRE</h3>
+            <p className='descripcion-seccion'>Ropa de hombre y accesorios de las más recientes colecciones en The Outfit.</p>
+        </section>
+
+        <section className="seccion-catalogo">
+            <PaginaCategoria idCategoriaPadre={57} />
+        </section>
+
+            </main>
+            <Footer />
+        </>
+    )
+}
